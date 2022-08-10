d1eb49478f0fa4ff4a92a4d24ad199a7f8f413b5
Jonny Li <myself@jonny.li>
2022-08-09 22:58:53 -0400

    Add Reddit data retrieval/preprocessing scripts

---
+++ scripts/preprocess_reddit_data.py
@@ -0,0 +1,58 @@
+import argparse
+import re
+import string
+import os
+import glob
+import unidecode
+import nltk
+import emoji
+
+sent_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+punct = r'!"#$%()*+,-./:;<=>?@[\\]^_`{|}~'
+punct_dict = str.maketrans(punct, ' ' * len(punct))
+url_re = re.compile(r'http\S+')
+space_re = re.compile(r'\s+')
+tag_re = re.compile(r'(^|\s)[#\$@/*]\w+')
+word_re = re.compile(r'^[a-z]\D+[a-z]$')
+reddit_re = re.compile(r'(r|u)/\w+')
+repeat_chars_re = re.compile(r'([a-z])\1{2,}')
+
+def clean(line):
+    line = line.strip().lower()
+    line = unidecode.unidecode(line)
+    line = emoji.replace_emoji(line, replace='')
+    line = url_re.sub('', line)
+    line = tag_re.sub('', line)
+    line = reddit_re.sub('', line)
+    line = repeat_chars_re.sub(r'\1', line)
+    line = line.replace(' & ', ' and ')
+    line = line.replace('[removed]', '').replace('[deleted]', '')
+    sents = []
+    for sent in sent_tokenizer.tokenize(line):
+        sent = sent.translate(punct_dict)
+        words = [word.strip("'") for word in sent.split()]
+        words = [word for word in words if word_re.match(word)]
+        sent = ' '.join(word for word in words)
+        if sent:
+            sents.append(sent)
+    return sents
+
+def main(args):
+    os.makedirs(args.output_dir, exist_ok=True)
+    files = glob.glob('**/*.txt', root_dir=args.input_dir, recursive=True)
+    for file in files:
+        input_file = os.path.join(args.input_dir, file)
+        with open(input_file, 'r', encoding='utf-8') as f:
+            cleaned_lines = [sent for line in f for sent in clean(line)]
+        output_file = os.path.join(args.output_dir, file)
+        with open(output_file, 'w', encoding='utf-8') as f:
+            f.writelines(f'{line}\n' for line in cleaned_lines)
+        print(f'Wrote {len(cleaned_lines)} lines to {output_file}')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_dir')
+    parser.add_argument('output_dir')
+    args = parser.parse_args()
+
+    main(args)
---
+++ scripts/reddit_crawler.py
@@ -0,0 +1,63 @@
+import argparse
+import json
+import os
+import re
+import praw
+import markdown
+from bs4 import BeautifulSoup
+
+SPACE_RE = re.compile(r'\s+')
+
+def clean(line):
+    line = markdown.markdown(line)
+    line = BeautifulSoup(line, 'html.parser').get_text()
+    line = line.replace('[deleted]', '').replace('[removed]', '')
+    line = SPACE_RE.sub(' ', line).strip()
+    return line
+
+def write_to_file(subreddit, lines):
+    clean_lines = []
+    for line in lines:
+        clean_line = clean(line)
+        if clean_line:
+            clean_lines.append(clean_line)
+    out_path = os.path.join(args.output_dir, f'{subreddit}.txt')
+    with open(out_path, 'a', encoding='utf-8') as f:
+        f.writelines(f'{line}\n' for line in clean_lines)
+
+def main(args):
+    with open('data/reddit_crawler_config.json', 'r') as f:
+        config = json.load(f)
+    reddit = praw.Reddit(**config)
+
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    for subreddit_name in args.subreddits:
+        subreddit = reddit.subreddit(subreddit_name)
+        for submission in subreddit.hot(limit=args.limit):
+            lines = [submission.title]
+            if submission.selftext:
+                lines.append(submission.selftext)
+            while True:
+                try:
+                    submission.comments.replace_more(limit=None)
+                    break
+                except e:
+                    print(e)
+            comments = submission.comments.list()
+            for comment in comments:
+                lines.append(comment.body_html)
+                if len(lines) >= 1000:
+                    write_to_file(subreddit_name, lines)
+                    lines.clear()
+            if lines:
+                write_to_file(subreddit_name, lines)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('output_dir')
+    parser.add_argument('subreddits', nargs='+')
+    parser.add_argument('--limit', default=1000)
+    args = parser.parse_args()
+
+    main(args)
